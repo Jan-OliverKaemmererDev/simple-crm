3818e461eb3493ce15dfa283655e97197d9c0fd8
Jan-Oliver <jowieja22@gmail.com>
2026-06-04 14:13:25 +0200

    feat: implement Angular Material sidenav and toolbar in app layout
    - Replaced placeholder content in app.html with a responsive sidenav and toolbar.
    - Added necessary Angular Material modules for toolbar, icon, button, and sidenav.
    - Updated app.scss to style the new layout components, including toolbar and sidenav.
    - Introduced a toggle button for additional content in the sidenav.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:
+  [ RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('simple-crm');
+  showFiller = false;
 }
